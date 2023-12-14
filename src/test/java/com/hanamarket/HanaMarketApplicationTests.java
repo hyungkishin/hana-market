0b989fd104b5b7567a2036418afa7c832f85e047
신형기 <tkaqkeldk@hanatour.com>
2023-12-14 23:32:20 +0900

    fix: yml update

--- src/test/java/com/hanamarket/HanaMarketApplicationTests.java
+++ src/test/java/com/hanamarket/HanaMarketApplicationTests.java
@@ -3,10 +3,10 @@ package com.hanamarket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class HanaMarketApplicationTests {
 
-//    @Test
+    @Test
     void contextLoads() {
     }
 
