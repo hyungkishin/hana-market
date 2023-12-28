c5637df94a8cd5894dbd043ea8599fe8b08295e0
eojin312 <eojin312@naver.com>
2023-12-28 17:03:50 +0900

    사용하지않는 주입은 제거

--- src/main/java/com/hanamarket/config/security/SecurityService.java
+++ src/main/java/com/hanamarket/config/security/SecurityService.java
@@ -9,17 +9,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
 public class SecurityService implements UserDetailsService {
-
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findByEmail(username)
